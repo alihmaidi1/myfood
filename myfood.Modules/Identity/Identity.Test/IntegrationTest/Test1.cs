a0282594736d082f9a1da1b65bbe62d6dbd03439
alihmaidi1 <alihmaidi095@gmail.com>
2025-07-15 12:05:34 -0400

    fix: remove integration test setup

--- myfood.Modules/Identity/Identity.Test/IntegrationTest/Test1.cs
+++ myfood.Modules/Identity/Identity.Test/IntegrationTest/Test1.cs
@@ -1,17 +1,17 @@
-namespace Identity.Test.IntegrationTest;
-
-public class Test1:BaseIntegrationTest
-{
-    public Test1(IntegrationTestWebAppFactory factory) : base(factory)
-    {
-        
-    }
-
-    [Fact]
-    public async Task Create_ShouldCreateProduct()
-    {
-        var user = _DbContext.Users.ToList();
-        var t = true;
-    }
-
-}
+// namespace Identity.Test.IntegrationTest;
+//
+// public class Test1:BaseIntegrationTest
+// {
+//     public Test1(IntegrationTestWebAppFactory factory) : base(factory)
+//     {
+//         
+//     }
+//
+//     [Fact]
+//     public async Task Create_ShouldCreateProduct()
+//     {
+//         var user = _DbContext.Users.ToList();
+//         var t = true;
+//     }
+//
+// }
